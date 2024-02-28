63c5adc64f15cc276e5855732e27f048c9efdf63
Natallia Burtina <nata330@gmail.com>
2024-02-28 09:04:11 -0800

    Fixed names of functions.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 console.log('Lesson 2');
 console.log('----------------------------');
+
+
 let user = 'John Doe';
 console.log(user);
 const student = 'Natallia Burtina';
@@ -7,6 +9,8 @@ console.log(student);
 user = student; //expected outcome: Natallia Burtina
 console.log(user);
 console.log('----------------------------');
+
+
 let test = 1;
 test += '1'; //expected outcome: 11
 console.log(test);
@@ -15,83 +19,104 @@ console.log(test);
 test = Boolean(test); // expected outcome: true
 console.log(test);
 console.log('----------------------------');
+
+
 let num = [2, 3, 5, 8];
 let product = 1;
+
 for (let i = 0; i < num.length; i++) {
   product = product * num[i];
 }
+
 console.log('Произведение элементов этого массива равно', product);
 console.log('----------------------------');
 
+
 num = [2, 5, 8, 15, 0, 6, 20, 3];
 console.log('Элементы массива, которые больше 5-ти, но меньше 10-ти:');
+
 for (let i = 0; i < num.length; i++) {
   if (num[i] > 5 && num[i] < 10) {
     console.log(num[i]);
   }
 }
+
 console.log('----------------------------');
 
 num = [2, 5, 8, 15, 0, 6, 20, 3];
 console.log('Четные элементы массива:');
+
 for (let i = 0; i < num.length; i++) {
   if (num[i] % 2 === 0) {
     console.log(num[i]);
   }
 }
+
 console.log('----------------------------------------');
 
 console.log('Lesson 3');
 console.log('----------------------------');
 function palindrome(word) {
   let word1 = '';
+
   for (let i = 0; i < word.length; i++) {
     word1 += word[word.length - 1 - i];
   }
+
   if (word === word1) {
     return 'true';
-  } else return 'false';
+  } else {
+    return 'false';
+  }
 }
 
 console.log(palindrome('racecar'));
 console.log('----------------------------');
+
 function min(a, b) {
   if (a < b) {
     return a;
-  } else return b;
+  } else {
+    return b;
+  }
 }
 
 console.log('Меньшее из чисел', min(5, 7));
 
-function min1(a, b) {
+function minWithTernaryOperator(a, b) {
   return a < b ? a : b;
 }
 
-console.log('Меньшее из чисел', min1(5, 7));
-
+console.log('Меньшее из чисел', minWithTernaryOperator(5, 7));
 console.log('----------------------------');
 
 function max(a, b) {
   if (a > b) {
     return a;
-  } else return b;
+  } else {
+    return b;
+  }
 }
 
 console.log('Большее из чисел', max(5, 7));
 
-function max1(a, b) {
+function maxWithTernaryOperator(a, b) {
   return a > b ? a : b;
 }
 
-console.log('Большее из чисел', max1(5, 7));
+console.log('Большее из чисел', maxWithTernaryOperator(5, 7));
 console.log('----------------------------');
 
+
 const arr = [];
+
 for (let i = 0; i < 10; i++) {
-  arr.push(Math.floor(Math.random() * 100)+1);
+  arr.push(Math.floor(Math.random() * 100) + 1);
 }
+
 console.log('Начальный массив ', arr);
-function zero(arr) {
+
+function replaceZeroWithString(arr) {
   for (let i = 0; i < 10; i++) {
     arr[i] = arr[i] + '';
     arr[i] = arr[i].replace(0, 'zero');
@@ -103,4 +128,4 @@ function zero(arr) {
   return arr;
 }
 
-console.log('Полученный массив ', zero(arr));
+console.log('Полученный массив ', replaceZeroWithString(arr));
