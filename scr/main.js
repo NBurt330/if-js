fe9356c4aeb02d1f25504e530099d97e123259e8
Natallia Burtina <nata330@gmail.com>
2024-03-22 09:21:23 -0700

    Added functions changeDateFormat, stringSearch, palindromeNew, matchCountryWithCities, getCalendarMonth.

--- scr/main.js
+++ scr/main.js
@@ -1,5 +1,6 @@
 import { sum } from './sum.js';
 import { getColor } from './colors.js';
+import { changeDateFormat } from './sum.js';
 
 console.log('Lesson 2');
 console.log('----------------------------');
@@ -133,7 +134,7 @@ console.log('----------------------------------------');
 console.log('Lesson 4');
 console.log('----------------------------');
 
-console.log('Суммва двух чисел равна ', sum(5)(2)); // 7
+console.log('Сумма двух чисел равна ', sum(5)(2)); // 7
 console.log('----------------------------');
 
 export const colors = [
@@ -154,3 +155,308 @@ for (let i = 0; i <= paragraph.length; i++) {
     paragraph[i].addEventListener('click', getColor(paragraph[i]));
   }
 }
+
+console.log('----------------------------------------');
+
+console.log('Lesson 5');
+console.log('----------------------------');
+
+const date = '2020-11-26';
+console.log('Введенная дата', date);
+console.log('Дата в новом формате', changeDateFormat(date));
+console.log('----------------------------');
+
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+function stringSearch(str) {
+  const result = [];
+  for (let i = 0; i < data.length; i++) {
+    const itemSearch = `${data[i].country}, ${data[i].city}, ${data[i].hotel}`;
+    if (itemSearch.includes(str)) {
+      result.push(itemSearch);
+    }
+  }
+  return result.length > 0 ? result : 'No matches';
+}
+
+let str = 'Berlin';
+console.log('Поиск по слову', str, stringSearch(str));
+
+console.log('----------------------------------------');
+
+console.log('Lesson 6');
+console.log('----------------------------');
+
+function palindromeNew(word) {
+  return word === word.split().reverse().join();
+}
+
+const word = 'шалаш';
+console.log(`Является ли слово ${word} палиндромом:`, palindromeNew(word));
+console.log('----------------------------');
+
+const hotels = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa Cruz de Tenerife',
+    country: 'Spain',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Virgin Hotel',
+    city: 'Chicago',
+    country: 'USA',
+  },
+  {
+    name: 'Grand Beach Resort',
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+  },
+  {
+    name: 'Shilla Stay',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    name: 'San Firenze Suites',
+    city: 'Florence',
+    country: 'Italy',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Black Penny Villas',
+    city: 'BTDC, Nuca Dua',
+    country: 'Indonesia',
+  },
+  {
+    name: 'Koko Hotel',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    name: 'Ramada Plaza',
+    city: 'Istanbul',
+    country: 'Turkey',
+  },
+  {
+    name: 'Waikiki Resort Hotel',
+    city: 'Hawaii',
+    country: 'USA',
+  },
+  {
+    name: 'Puro Hotel',
+    city: 'Krakow',
+    country: 'Poland',
+  },
+  {
+    name: 'Asma Suites',
+    city: 'Santorini',
+    country: 'Greece',
+  },
+  {
+    name: 'Cityden Apartments',
+    city: 'Amsterdam',
+    country: 'Netherlands',
+  },
+  {
+    name: 'Mandarin Oriental',
+    city: 'Miami',
+    country: 'USA',
+  },
+  {
+    name: 'Concept Terrace Hotel',
+    city: 'Rome',
+    country: 'Italy',
+  },
+  {
+    name: 'Ponta Mar Hotel',
+    city: 'Fortaleza',
+    country: 'Brazil',
+  },
+  {
+    name: 'Four Seasons Hotel',
+    city: 'Sydney',
+    country: 'Australia',
+  },
+  {
+    name: 'Le Meridien',
+    city: 'Nice',
+    country: 'France',
+  },
+  {
+    name: 'Apart Neptun',
+    city: 'Gdansk',
+    country: 'Poland',
+  },
+  {
+    name: 'Lux Isla',
+    city: 'Ibiza',
+    country: 'Spain',
+  },
+  {
+    name: 'Nox Hostel',
+    city: 'London',
+    country: 'UK',
+  },
+  {
+    name: 'Leonardo Vienna',
+    city: 'Vienna',
+    country: 'Austria',
+  },
+  {
+    name: 'Adagio Aparthotel',
+    city: 'Edinburgh',
+    country: 'UK',
+  },
+  {
+    name: 'Steigenberger Hotel',
+    city: 'Hamburg',
+    country: 'Germany',
+  },
+];
+
+function newStringSearch(str) {
+  const result = [];
+  hotels.forEach((arr) => {
+    const itemSearch = `${arr.country}, ${arr.city}, ${arr.name}`;
+    if (itemSearch.includes(str)) {
+      result.push(itemSearch);
+    }
+  });
+  return result;
+}
+
+str = 'USA';
+console.log('Поиск по слову', str, newStringSearch(str));
+console.log('----------------------------');
+
+function matchCountryWithCities(arr) {
+  const result = {};
+  arr.forEach((arr) => {
+    const { country, city } = arr;
+    if (result[country]) {
+      result[country].push(city);
+    } else {
+      result[country] = [city];
+    }
+  });
+  return result;
+}
+
+console.log(
+  'Уникальные страны и сопоставимые с ними города',
+  matchCountryWithCities(hotels),
+);
+console.log('----------------------------');
+
+function getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek) {
+  let numOfWeeks = 0;
+  if (dayOfWeek > 30 || dayOfWeek >= daysInWeek) {
+    return 'Wrong date';
+  } else {
+    numOfWeeks = Math.ceil((daysInMonth + dayOfWeek) / daysInWeek);
+    let result = [];
+    let count = daysInMonth - dayOfWeek + 1;
+    for (let i = 0; i < numOfWeeks; i++) {
+      const numOfWeek = [];
+      for (let j = 0; j < daysInWeek; j++) {
+        count <= daysInMonth
+          ? numOfWeek.push(count)
+          : numOfWeek.push(count - 30);
+        count++;
+        if (count >= 2 * daysInMonth) {
+          count = count - daysInMonth;
+        }
+      }
+      result = [...result, numOfWeek];
+    }
+    return result;
+  }
+}
+
+const daysInMonth = 30;
+const daysInWeek = 7;
+const dayOfWeek = 4;
+const calendarMonth = getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek);
+console.log(calendarMonth);
+
+console.log('----------------------------');
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,8 +1,24 @@
 import { sum } from '../scr/sum.js';
 import { randNumber } from '@ngneat/falso';
+import { changeDateFormat } from '../scr/sum.js';
 
 test('sum of two numbers', () => {
   const num1 = randNumber();
   const num2 = randNumber();
   expect(sum(num1)(num2)).toBe(num1 + num2);
+});
+
+test('format of date should be day.month.year', () => {
+  const day = randNumber({ min: 1, max: 31 });
+  const month = randNumber({ min: 1, max: 12 });
+  const year = randNumber({ min: 1, max: 2024 });
+  const date = year + '-' + month + '-' + day;
+  const newDate = changeDateFormat(date);
+  const testDate = newDate.split('.');
+  expect(Number(testDate[0])).toBeGreaterThanOrEqual(1);
+  expect(Number(testDate[0])).toBeLessThanOrEqual(31);
+  expect(Number(testDate[1])).toBeGreaterThanOrEqual(1);
+  expect(Number(testDate[1])).toBeLessThanOrEqual(12);
+  expect(Number(testDate[2])).toBeGreaterThanOrEqual(1);
+  expect(Number(testDate[2])).toBeLessThanOrEqual(2024);
 });
