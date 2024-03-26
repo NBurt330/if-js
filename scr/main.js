4cdd46af618f42bff23ea8ee14544abaace86c8f
Natallia Burtina <nata330@gmail.com>
2024-03-26 00:29:49 -0700

    Fixed functions newStringSearch and matchCountryWithCities.

--- scr/main.js
+++ scr/main.js
@@ -1,6 +1,7 @@
-import { sum } from './sum.js';
+import { changeDateFormat, sum } from './sum.js';
 import { getColor } from './colors.js';
-import { changeDateFormat } from './sum.js';
+import { hotels } from './constants/hotels.js';
+import { data } from './constants/data.js';
 
 console.log('Lesson 2');
 console.log('----------------------------');
@@ -120,10 +121,12 @@ function replaceZeroWithString(arr) {
     arr[i] = arr[i] + '';
     arr[i] = arr[i].replace(0, 'zero');
     arr[i] = arr[i].replace(0, 'zero');
+
     if (arr[i].includes('zero') === false) {
       arr[i] = Number(arr[i]);
     }
   }
+
   return arr;
 }
 
@@ -166,57 +169,17 @@ console.log('Введенная дата', date);
 console.log('Дата в новом формате', changeDateFormat(date));
 console.log('----------------------------');
 
-const data = [
-  {
-    country: 'Russia',
-    city: 'Saint Petersburg',
-    hotel: 'Hotel Leopold',
-  },
-  {
-    country: 'Spain',
-    city: 'Santa Cruz de Tenerife',
-    hotel: 'Apartment Sunshine',
-  },
-  {
-    country: 'Slowakia',
-    city: 'Vysokie Tatry',
-    hotel: 'Villa Kunerad',
-  },
-  {
-    country: 'Germany',
-    city: 'Berlin',
-    hotel: 'Hostel Friendship',
-  },
-  {
-    country: 'Indonesia',
-    city: 'Bali',
-    hotel: 'Ubud Bali Resort&SPA',
-  },
-  {
-    country: 'Netherlands',
-    city: 'Rotterdam',
-    hotel: 'King Kong Hostel',
-  },
-  {
-    country: 'Marocco',
-    city: 'Ourika',
-    hotel: 'Rokoko Hotel',
-  },
-  {
-    country: 'Germany',
-    city: 'Berlin',
-    hotel: 'Hotel Rehberge Berlin Mitte',
-  },
-];
-
 function stringSearch(str) {
   const result = [];
+
   for (let i = 0; i < data.length; i++) {
     const itemSearch = `${data[i].country}, ${data[i].city}, ${data[i].hotel}`;
+
     if (itemSearch.includes(str)) {
       result.push(itemSearch);
     }
   }
+
   return result.length > 0 ? result : 'No matches';
 }
 
@@ -236,173 +199,22 @@ const word = 'шалаш';
 console.log(`Является ли слово ${word} палиндромом:`, palindromeNew(word));
 console.log('----------------------------');
 
-const hotels = [
-  {
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-  },
-  {
-    name: 'Apartment Sunshine',
-    city: 'Santa Cruz de Tenerife',
-    country: 'Spain',
-  },
-  {
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-  },
-  {
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-  },
-  {
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-  },
-  {
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-  },
-  {
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Virgin Hotel',
-    city: 'Chicago',
-    country: 'USA',
-  },
-  {
-    name: 'Grand Beach Resort',
-    city: 'Dubai',
-    country: 'United Arab Emirates',
-  },
-  {
-    name: 'Shilla Stay',
-    city: 'Seoul',
-    country: 'South Korea',
-  },
-  {
-    name: 'San Firenze Suites',
-    city: 'Florence',
-    country: 'Italy',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Black Penny Villas',
-    city: 'BTDC, Nuca Dua',
-    country: 'Indonesia',
-  },
-  {
-    name: 'Koko Hotel',
-    city: 'Tokyo',
-    country: 'Japan',
-  },
-  {
-    name: 'Ramada Plaza',
-    city: 'Istanbul',
-    country: 'Turkey',
-  },
-  {
-    name: 'Waikiki Resort Hotel',
-    city: 'Hawaii',
-    country: 'USA',
-  },
-  {
-    name: 'Puro Hotel',
-    city: 'Krakow',
-    country: 'Poland',
-  },
-  {
-    name: 'Asma Suites',
-    city: 'Santorini',
-    country: 'Greece',
-  },
-  {
-    name: 'Cityden Apartments',
-    city: 'Amsterdam',
-    country: 'Netherlands',
-  },
-  {
-    name: 'Mandarin Oriental',
-    city: 'Miami',
-    country: 'USA',
-  },
-  {
-    name: 'Concept Terrace Hotel',
-    city: 'Rome',
-    country: 'Italy',
-  },
-  {
-    name: 'Ponta Mar Hotel',
-    city: 'Fortaleza',
-    country: 'Brazil',
-  },
-  {
-    name: 'Four Seasons Hotel',
-    city: 'Sydney',
-    country: 'Australia',
-  },
-  {
-    name: 'Le Meridien',
-    city: 'Nice',
-    country: 'France',
-  },
-  {
-    name: 'Apart Neptun',
-    city: 'Gdansk',
-    country: 'Poland',
-  },
-  {
-    name: 'Lux Isla',
-    city: 'Ibiza',
-    country: 'Spain',
-  },
-  {
-    name: 'Nox Hostel',
-    city: 'London',
-    country: 'UK',
-  },
-  {
-    name: 'Leonardo Vienna',
-    city: 'Vienna',
-    country: 'Austria',
-  },
-  {
-    name: 'Adagio Aparthotel',
-    city: 'Edinburgh',
-    country: 'UK',
-  },
-  {
-    name: 'Steigenberger Hotel',
-    city: 'Hamburg',
-    country: 'Germany',
-  },
-];
-
 function newStringSearch(str) {
-  const result = [];
-  hotels.forEach((arr) => {
-    const itemSearch = `${arr.country}, ${arr.city}, ${arr.name}`;
-    if (itemSearch.includes(str)) {
-      result.push(itemSearch);
-    }
-  });
-  return result;
+  const result = hotels.filter((elem) =>
+    `${elem.country}, ${elem.city}, ${elem.name}`.includes(str),
+  );
+
+  let resultStr = [];
+
+  result.forEach(
+    (elem) =>
+      (resultStr = [
+        ...resultStr,
+        `${elem.country}, ${elem.city}, ${elem.name}`,
+      ]),
+  );
+
+  return resultStr;
 }
 
 str = 'USA';
@@ -410,16 +222,15 @@ console.log('Поиск по слову', str, newStringSearch(str));
 console.log('----------------------------');
 
 function matchCountryWithCities(arr) {
-  const result = {};
-  arr.forEach((arr) => {
-    const { country, city } = arr;
-    if (result[country]) {
-      result[country].push(city);
+  return arr.reduce((acc, elem) => {
+    if (Object.keys(acc).includes(elem.country)) {
+      acc[elem.country] = [...acc[elem.country], elem.city];
+      return acc;
     } else {
-      result[country] = [city];
+      acc[elem.country] = [elem.city];
+      return acc;
     }
-  });
-  return result;
+  }, []);
 }
 
 console.log(
@@ -430,25 +241,31 @@ console.log('----------------------------');
 
 function getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek) {
   let numOfWeeks = 0;
+
   if (dayOfWeek > 30 || dayOfWeek >= daysInWeek) {
     return 'Wrong date';
   } else {
     numOfWeeks = Math.ceil((daysInMonth + dayOfWeek) / daysInWeek);
     let result = [];
     let count = daysInMonth - dayOfWeek + 1;
+
     for (let i = 0; i < numOfWeeks; i++) {
       const numOfWeek = [];
+
       for (let j = 0; j < daysInWeek; j++) {
         count <= daysInMonth
           ? numOfWeek.push(count)
           : numOfWeek.push(count - 30);
         count++;
+
         if (count >= 2 * daysInMonth) {
           count = count - daysInMonth;
         }
       }
+
       result = [...result, numOfWeek];
     }
+
     return result;
   }
 }
